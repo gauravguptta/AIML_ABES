e6381af77364b5ca41c4ad1819de9b431c64e565
gaurav gupta <gaurav.23b1533013@abes.ac.in>
2025-02-07 09:24:37 +0530

    updated work 7/2/25

---
+++ AIML/Back/Backend/File system/file1.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// const data = fs.readFileSync("./data.txt");
+// console.log(data.toString()); // converting in string 
+const data = fs.readFileSync("./data.txt","utf-8");
+console.log(data);
+
+
+
+
