6ffcb4c25a8e0919d792d942af391267a74511fa
gaurav gupta <gaurav.23b1533013@abes.ac.in>
2025-02-07 09:47:34 +0530

    updated work 7/2/25

--- AIML/Back/Backend/File system/file1.js
+++ AIML/Back/Backend/File system/file1.js
@@ -4,6 +4,18 @@ const fs = require('fs');
 const data = fs.readFileSync("./data.txt","utf-8");
 console.log(data);
 
+fs.writeFileSync("./data1.txt","hey this is my new work","utf-8");
+
+// fs.appendFileSync("./data1.txt","work from another file","utf-8");
+// fs.renameSync("./data3.txt","./data5.txt");
+// fs.unlinkSync("./data5.txt");
+console.log(data);
+if(data.match("H")){
+    const newdata = data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
+
+
 
 
 
